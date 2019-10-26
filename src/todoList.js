5e7c38523a3b456583b3058ec1b035c349ea4dfd
WolfGirlM <18829491124@163.com>
2019-10-26 14:34:18 +0800

    :art: 升级为V16.6.0语法，使用函数组件，新增useState，优化this指向问题

--- src/todoList.js
+++ src/todoList.js
@@ -1,56 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Button,Layout,Card,Input} from 'antd'
 import TodoItem from './TodoItem'
 
 const { Header,Content } = Layout;
-class TodoList extends Component {
-  state = {
-    list: [],
-    inputValue: ''
+
+const TodoList = () => {
+  const [list, setList] = useState([])
+  const [inputValue, setInputValue] = useState('')
+
+  const handleBtnClick = () => {
+    setList([...list, inputValue])
+    setInputValue('')
   }
-  handleBtnClick = () => {
-    this.setState({
-      list: [...this.state.list, this.state.inputValue],
-      inputValue: ''
-    })
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
   }
-  handleInputChange = (e) => {
-    this.setState({
-      inputValue: e.target.value
-    })
+
+  const deleteItem = (index) => {
+    const temp = [...list];
+    temp.splice(index, 1);
+    setList(temp)
   }
-  deleteItem = (index) => {
-    const list = [...this.state.list];
-    list.splice(index,1);
-    this.setState({
-      list: list
-    })
-  }
-  renderTodoItems = () => this.state.list.map((item, index) =>
+
+  const renderTodoItems = () => list.map((item, index) =>
     <TodoItem 
       key = { index }
-      content = {item}
-      delete = { () => { this.deleteItem(index) }}
+      content = { item }
+      delete = { () => { deleteItem(index) }}
     />
   )
   //JSX
-  render() {
-    const { inputValue } = this.state
-    return (
-      < Layout theme="light">
-        <Header>@狼果驿站</Header>
-        <Content>
-          <Card title="To do List @React">
-              <div className="tagGroup">
-              <Input value = { inputValue } onChange = { this.handleInputChange }/>
-              <Button type = "primary" onClick={ this.handleBtnClick } className='add-btn'> 创建任务 </Button>
-            </div>
-            <ul>{this.renderTodoItems() }</ul>
-          </Card>
-        </Content>
-      </Layout>
-    );
-  }
+  return (
+    < Layout theme="light">
+      <Header>@狼果驿站</Header>
+      <Content>
+        <Card title="To do List @React">
+            <div className="tagGroup">
+            <Input value = { inputValue } onChange = { handleInputChange }/>
+            <Button type = "primary" onClick={ handleBtnClick } className='add-btn'> 创建任务 </Button>
+          </div>
+          <ul>{ renderTodoItems() }</ul>
+        </Card>
+      </Content>
+    </Layout>
+  )
 }
 
 export default TodoList;
